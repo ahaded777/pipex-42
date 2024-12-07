4936428ddbdcc5bb51ba8938bf2b0f455dbb6f8c
7NL <131011542+ahaded777@users.noreply.github.com>
2024-12-07 18:50:31 +0100

    Update pipex_utils.c

--- pipex_utils.c
+++ pipex_utils.c
@@ -6,27 +6,16 @@
 /*   By: aahaded <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 21:11:01 by aahaded           #+#    #+#             */
-/*   Updated: 2024/12/06 12:03:28 by aahaded          ###   ########.fr       */
+/*   Updated: 2024/12/07 18:22:24 by aahaded          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "pipex.h"
 
-void	find_path_child(int *pipefd, char **argv)
-{
-	close(pipefd[0]);
-	dup2(pipefd[1], STDOUT_FILENO);
-	close(pipefd[1]);
-	execve("/usr/bin/which", argv, NULL);
-	perror("Error executing cmd");
-	exit(1);
-}
-
-char	*find_path_parent(char **result, int *pipefd, char **argv)
+char	*find_path_child(char **result, int *pipefd, char **argv)
 {
 	int	len_buff;
 
 	close(pipefd[1]);
-	wait(NULL);
 	*result = malloc(1024);
 	if (!*result)
 		return (free(argv), NULL);
@@ -35,6 +24,16 @@ char	*find_path_parent(char **result, int *pipefd, char **argv)
 		return (free(argv), free(*result), NULL);
 	close(pipefd[0]);
 	return (*result);
+}
+
+void	find_path_parent(int *pipefd, char **argv)
+{
+	close(pipefd[0]);
+	dup2(pipefd[1], STDOUT_FILENO);
+	close(pipefd[1]);
+	execve("/usr/bin/which", argv, NULL);
+	perror("Error executing cmd");
+	exit(1);
 }
 
 void	execute_command(char *cmd)
